aa26c5f1d9a71c3a4eba6ad386b4336a11d57aa6
Theodore <elvisxiaozhi@gmail.com>
2018-08-27 23:50:53 +0800

    remove old files

--- Server/Source/connection.cpp
+++
@@ -1,7 +0,0 @@
-#include "connection.h"
-
-Connection::Connection(QObject *parent) : QObject(parent)
-{
-    tcpServer = new QTcpServer; //can not set parent there, or error will show
-    tcpServer->listen(QHostAddress::Any, 6666);
-}
