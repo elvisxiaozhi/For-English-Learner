776b8f241c18dddd7bc985ca70160d004e675ec1
Theodore <elvisxiaozhi@gmail.com>
2018-08-28 16:58:11 +0800

    add tool btn style sheet

--- src/widget.cpp
+++ src/widget.cpp
@@ -19,7 +19,13 @@ void Widget::setWidgetLayout()
 {
     setWindowTitle("Tic Tac Toe");
 
+    ui->cross->setIcon(QIcon(":/icons/cross.png"));
+    ui->circle->setIcon(QIcon(":/icons/circle.png"));
+
     setStyleSheet(
+                "QToolButton { background: white; border: 0px; padding: 5px 20px 2px 30px; }"
+                ".QToolButton#cross { border-bottom: 3px solid #00cccc; }"
+                ".QToolButton#circle { border-bottom: 3px solid white; }"
                 "QLabel#msLbl { color: #666666; font: 13px; }"
                 "QWidget#chessboard { background: #00cccc; }"
                 "QPushButton#restartBtn { background-color: #F7F9F9; border: 0px; border-bottom: 3px solid #F7F9F9; padding: 23px 0px 17px 0px; "
